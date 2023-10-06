984731ededc48d083a42cc91efb6516e32fc3c01
lk3v1nn <kevinlmorataya@gmail.com>
2023-10-06 08:35:58 -0600

    footer agregado

--- HDT-9/src/App.js
+++ HDT-9/src/App.js
@@ -5,22 +5,26 @@ import { Hola } from "./modules/Inicio.js";
 import { ApiCats } from "./modules/ApiCats.js";
 import { ApiDogs } from "./modules/ApiDogs";
 import { ListaPokemon } from "./modules/ListaPokemon";
+import { Footer } from "./modules/Footer";
 import { Route, Routes } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Navegador />
-      <Routes>
-        <Route path="/" element={<Hola />} />
-        <Route path="/cats" element={<ApiCats />} />
-        <Route path="/dogs" element={<ApiDogs />} />
-        <Route path="/pokemons" element={<ListaPokemon />} />
-      </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navegador />
+            <div className="main">
+                <Routes>
+                    <Route path="/" element={<Hola />} />
+                    <Route path="/cats" element={<ApiCats />} />
+                    <Route path="/dogs" element={<ApiDogs />} />
+                    <Route path="/pokemons" element={<ListaPokemon />} />
+                </Routes>
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
