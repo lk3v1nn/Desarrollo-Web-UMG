3f9a4e00e977c8a8ade868bcdf62fd3d609a4271
lk3v1nn <kevinlmorataya@gmail.com>
2023-10-27 21:11:33 -0600

    tarjetas del carrito arregladas

--- Proyecto-2/src/app/carrito/components/TarjetaCarrito.tsx
+++ Proyecto-2/src/app/carrito/components/TarjetaCarrito.tsx
@@ -2,7 +2,7 @@ export default function TarjetaCarrito() {
     return (
         <a
             href="#"
-            className="w-full m-1 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row max-w-xl hover:bg-gray-100 bg-white border-gray-200  bg-opacity-50"
+            className="w-full mt-2 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100 bg-white border-gray-200  bg-opacity-50"
         >
             <img
                 className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
@@ -10,10 +10,11 @@ export default function TarjetaCarrito() {
                 alt=""
             ></img>
             <div className="flex flex-col justify-between p-4 leading-normal">
-                <h5 className="mb-2 text-xl tracking-tight text-indigo-600">
+                <h5 className="mb-2 text-m tracking-tight text-indigo-600">
                     Nombre
                 </h5>
                 <p className="mb-3 font-normal text-gray-700 ">Descripcion</p>
+                <p className="mb-3 font-normal text-gray-700 ">Cantidad</p>
                 <button className="relative  px-4 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
                     Eliminar
                 </button>
