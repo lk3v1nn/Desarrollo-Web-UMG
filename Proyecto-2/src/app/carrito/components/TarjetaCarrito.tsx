3750c5e578e2e5f82978e5236a3380689365bb19
lk3v1nn <kevinlmorataya@gmail.com>
2023-10-31 16:37:25 -0600

    tarjeta carrito estructurada

--- Proyecto-2/src/app/carrito/components/TarjetaCarrito.tsx
+++ Proyecto-2/src/app/carrito/components/TarjetaCarrito.tsx
@@ -4,27 +4,20 @@ export default function TarjetaCarrito(prop: {
     nombre: string;
     descripcion: string;
     cantidad: string;
-    precio: String;
+    precio: string;
 }) {
-
     const axiosInstance = axios.create({
-        withCredentials: true
-    })
+        withCredentials: true,
+    });
     const eliminarProducto = () => {
-            axiosInstance.post('')
-            
-        }
-    }
+        axiosInstance.post("");
+    };
+
     return (
         <a
             href="#"
-            className="w-full mt-2 flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100 bg-white border-gray-200  bg-opacity-50"
+            className="w-full mt-2 grid grid-cols-2 bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100 bg-white border-gray-200  bg-opacity-50"
         >
-            <img
-                className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-                src="https://c1.klipartz.com/pngpicture/669/504/sticker-png-building-investment-percentage-venture-capital-funding-rate-business-discounts-and-allowances.png"
-                alt=""
-            ></img>
             <div className="flex flex-col justify-between p-4 leading-normal">
                 <h5 className="mb-2 text-m tracking-tight text-indigo-600">
                     {prop.nombre}
@@ -32,11 +25,13 @@ export default function TarjetaCarrito(prop: {
                 <p className="mb-3 font-normal text-gray-700 ">
                     {prop.descripcion}
                 </p>
-                <p className="mb-3 font-normal text-gray-700 ">{prop.precio}</p>
+                <p className="mb-3 font-normal text-gray-700 ">Q {prop.precio}</p>
                 <p className="mb-3 font-normal text-gray-700 ">
-                    {prop.cantidad}
+                    Cantidad: {prop.cantidad}
                 </p>
-                <button className="relative  px-4 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+            </div>
+            <div className="flex justify-end items-stretch">
+                <button className="relative  px-4 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-blue-300 bg-opacity-50">
                     Eliminar
                 </button>
             </div>
